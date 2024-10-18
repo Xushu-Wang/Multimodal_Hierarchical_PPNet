f5d304f6f5d6847b09e9a0d22a9e9da9c67259b5
Charlie Berens <charliejb3@gmail.com>
2024-10-17 23:46:26 -0400

    Who knows

--- train_multi.sh
+++ train_multi.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=150_Lite_MultiFullTest_Weights      # Job name
+#SBATCH --job-name=150_Lite_MultiFullTest_Weights_NEW      # Job name
 #SBATCH --ntasks=1                    # Run on a single Node
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80gb                  # Job memory request
 #SBATCH --time=48:00:00                # Time limit hrs:min:sec
 #SBATCH --partition=compsci-gpu
 #SBATCH --gres=gpu:1
-#SBATCH --output=logs/push_fix/150_Lite_MultiFullTest_Weights_%j.out
+#SBATCH --output=logs/push_fix/150_Lite_MultiFullTest_Weights_NEW_%j.out
 
 eval "$(conda shell.bash hook)" 
 conda activate intnn
