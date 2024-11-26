Added correct accuracy calculation. Added scripts for species training

dataio/tree.py
@@ -15,6 +15,7 @@ from torchvision.transforms import v2, ToTensor, transforms
 import tqdm
 import json
 from torchvision.transforms.functional import center_crop
+from torchvision.utils import save_image
 
 from prototype.push import get_train_dir_size
 
@@ -119,7 +120,7 @@ class TreeDataset(Dataset):
     mode 3 - returns (genetic_tensor, image_tensor), label
     """
 
-    def __init__(self, source_df:str, image_root_dir: str, class_specification, image_transforms, genetic_transforms, mode=3):
+    def __init__(self, source_df:str, image_root_dir: str, class_specification, image_transforms, genetic_transforms, mode=3, flat_class=False):
         self.df = source_df
         self.image_root_dir = image_root_dir
 
@@ -130,6 +131,12 @@ class TreeDataset(Dataset):
         self.one_hot_encoder = GeneticOneHot(length=720, zero_encode_unknown=True, include_height_channel=True)
         self.image_transforms = image_transforms
         self.genetic_transforms = genetic_transforms
+
+        # If flat_class is true, the label will be an integer, with each species having a unique integer.
+        self.flat_class = flat_class
+
+        if flat_class:
+            self.leaf_indicies, self.class_count = self.generate_leaf_indicies(self.tree, {"val": 0}) 
 
     def generate_tree_indicies(self, tree:dict, idx=0):
         """
@@ -152,6 +159,17 @@ class TreeDataset(Dataset):
             idx += 1
 
         return tree
+    
+    def generate_leaf_indicies(self, tree, idx):
+        tree = {k: v for k,v in tree.items()}
+        for k, v in tree.items():
+            if v == None:
+                tree[k] = {"idx": idx["val"]}
+                idx["val"] += 1
+            else:
+                tree[k] = self.generate_leaf_indicies(v, idx)[0]
+        
+        return tree, idx["val"]
 
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
@@ -181,21 +199,33 @@ class TreeDataset(Dataset):
 
         0 represents not_classified (or ignored, like in cases with only one class)
         """
-        tensor = torch.zeros(len(self.levels))
-        tree = self.indexed_tree
+        if self.flat_class:
+            tree = self.leaf_indicies
 
-        for i, level in enumerate(self.levels):
-            if row[level] == "not_classified" or row[level] not in tree:
-                tensor[i:] = 0
-                break
-            else:
-                tensor[i] = tree[row[level]]["idx"] + 1
+            for i, level in enumerate(self.levels):
+                if row[level] == "not_classified" or row[level] not in tree:
+                    raise ValueError("Somehow you got not classified's up in here. That's not supported in this mode.")
+                
+                if i == len(self.levels)-1:
+                    return torch.tensor(tree[row[level]]["idx"]).long()
+                
                 tree = tree[row[level]]
-        
-        # Convert float tensor to int tensor
-        tensor = tensor.long()
+        else:
+            tensor = torch.zeros(len(self.levels))
+            tree = self.indexed_tree
 
-        return tensor
+            for i, level in enumerate(self.levels):
+                if row[level] == "not_classified" or row[level] not in tree:
+                    tensor[i:] = 0
+                    break
+                else:
+                    tensor[i] = tree[row[level]]["idx"] + 1
+                    tree = tree[row[level]]
+            
+            # Convert float tensor to int tensor
+            tensor = tensor.long()
+
+            return tensor
 
     def __len__(self):
         return len(self.df)
@@ -644,6 +674,7 @@ def create_tree_dataloaders(
         transform_std:tuple,
         mode:int,
         seed=2024,
+        flat_class=False,
         log=print
 ):
     """
@@ -755,7 +786,8 @@ def create_tree_dataloaders(
         class_specification=class_specification,
         image_transforms=augmented_img_transforms,
         genetic_transforms=augmented_genetic_transforms,
-        mode=mode
+        mode=mode,
+        flat_class=flat_class
     )
     train_push_dataset = TreeDataset(
         source_df=train_push_df,
@@ -763,7 +795,8 @@ def create_tree_dataloaders(
         class_specification=class_specification,
         image_transforms=push_img_transforms,
         genetic_transforms=None,
-        mode=mode
+        mode=mode,
+        flat_class=flat_class
     )
 
     val_dataset = TreeDataset(
@@ -772,7 +805,8 @@ def create_tree_dataloaders(
         class_specification,
         img_transforms,
         None,
-        mode
+        mode,
+        flat_class=flat_class
     )
     test_dataset = TreeDataset(
         test_df,
@@ -780,30 +814,29 @@ def create_tree_dataloaders(
         class_specification,
         img_transforms,
         None,
-        mode
+        mode,
+        flat_class=flat_class
     )
 
     train_loader = DataLoader(
             train_dataset, batch_size=train_batch_size, shuffle=True,
-            num_workers=2, pin_memory=False, collate_fn=collate_fn,
-            
+            num_workers=1, pin_memory=False, collate_fn=collate_fn,       
     )
-    
     train_push_loader = DataLoader(
             train_push_dataset, batch_size=train_push_batch_size, shuffle=True,
-            num_workers=2, pin_memory=False, collate_fn=collate_fn)
+            num_workers=1, pin_memory=False, collate_fn=collate_fn)
     
     val_loader = DataLoader(
             val_dataset, batch_size=test_batch_size, shuffle=False,
-            num_workers=2, pin_memory=False, collate_fn=collate_fn)
+            num_workers=1, pin_memory=False, collate_fn=collate_fn)
     
     test_loader = DataLoader(
             test_dataset, batch_size=test_batch_size, shuffle=False,
-            num_workers=2, pin_memory=False, collate_fn=collate_fn)  
+            num_workers=1, pin_memory=False, collate_fn=collate_fn)  
 
     return train_loader, train_push_loader, val_loader, test_loader, normalize
 
-def get_dataloaders(cfg, log):
+def get_dataloaders(cfg, log, flat_class=False):
     log("Getting Dataloaders")
 
     return create_tree_dataloaders(
@@ -825,7 +858,8 @@ def get_dataloaders(cfg, log):
         transform_std=cfg.DATASET.IMAGE.TRANSFORM_STD,
         mode=cfg.DATASET.MODE,
         seed=cfg.SEED,
-        log=log
+        log=log,
+        flat_class=flat_class
     )
 
 def remove_images_for_which_there_exists_no_file_in_the_directory(dfs, image_root_dir, log=print):

main.py
@@ -38,7 +38,7 @@ def main():
     try:
         train_loader, train_push_loader, val_loader, test_loader, image_normalizer = get_dataloaders(cfg, log)
         
-        tree_ppnet = construct_tree_ppnet(cfg).cuda()
+        tree_ppnet = construct_tree_ppnet(cfg).to("cuda")
 
         tree_ppnet_multi = torch.nn.DataParallel(tree_ppnet)
         # TODO - Handle multi models
@@ -66,8 +66,6 @@ def main():
             log('epoch: \t{0}'.format(epoch))
             
             # Warm up and Training Epochs
-            print("WALAA")
-
             if not args.validate:
                 if epoch < cfg.OPTIM.NUM_WARM_EPOCHS:
                     tnt.warm_only(model=tree_ppnet_multi, log=log)
@@ -111,7 +109,7 @@ def main():
                 parallel_mode=cfg.DATASET.PARALLEL_MODE,
             )
             save_model_w_condition(model=tree_ppnet, model_dir=cfg.OUTPUT.MODEL_DIR, model_name=str(epoch) + 'nopush', accu=prob_accu,
-                                        target_accu=0.70, log=log)
+                                        target_accu=0, log=log)
 
             if args.validate:
                 break
@@ -131,9 +129,10 @@ def main():
                     no_save=cfg.OUTPUT.NO_SAVE
                 )
                 prob_accu = tnt.test(model=tree_ppnet_multi, dataloader=val_loader,
-                                class_specific=class_specific, log=log, global_ce=cfg.OPTIM.GLOBAL_CROSSENTROPY,)
+                                class_specific=class_specific, log=log,
+                                global_ce=cfg.OPTIM.GLOBAL_CROSSENTROPY,parallel_mode=cfg.DATASET.PARALLEL_MODE)
                 save_model_w_condition(model=tree_ppnet, model_dir=cfg.OUTPUT.MODEL_DIR, model_name=str(epoch) + 'push',
-                                            target_accu=0.70, log=log, accu=prob_accu)
+                                            target_accu=0, log=log, accu=prob_accu)
 
                 # Optimize last layer
                 if cfg.MODEL.PRUNE and epoch >= cfg.OPTIM.PRUNE_START:
@@ -171,8 +170,7 @@ def main():
                     )
 
                 # Optimize last layer again
-                # for i in range(20):
-                for i in range(5):
+                for i in range(20):
                     log('iteration: \t{0}'.format(i))
                     _ = tnt.train(
                         model=tree_ppnet_multi,
@@ -192,7 +190,7 @@ def main():
                         global_ce=cfg.OPTIM.GLOBAL_CROSSENTROPY,
                         log=log
                     )
-                    save_model_w_condition(model=tree_ppnet, model_dir=cfg.OUTPUT.MODEL_DIR, model_name=str(epoch) + '_' + 'push', accu=prob_accu, target_accu=0.70, log=log)
+                    save_model_w_condition(model=tree_ppnet, model_dir=cfg.OUTPUT.MODEL_DIR, model_name=str(epoch) + '_' + 'push', accu=prob_accu, target_accu=0, log=log)
                     if tree_ppnet.mode == 3 and not cfg.DATASET.PARALLEL_MODE:
                         tnt.multi_last_layer(model=tree_ppnet_multi, log=log)
                         _ = tnt.train(
@@ -213,7 +211,7 @@ def main():
                             parallel_mode=cfg.DATASET.PARALLEL_MODE,
                             log=log
                         )
-                        save_model_w_condition(model=tree_ppnet, model_dir=cfg.OUTPUT.MODEL_DIR, model_name=str(epoch) + '_' + 'push', accu=prob_accu, target_accu=0.70, log=log)
+                        save_model_w_condition(model=tree_ppnet, model_dir=cfg.OUTPUT.MODEL_DIR, model_name=str(epoch) + '_' + 'push', accu=prob_accu, target_accu=0, log=log)
 
                 # Print the weights of the last layer
                 # Save the weigts of the last layer

model/model.py
@@ -163,5 +163,7 @@ def construct_tree_ppnet(cfg, log=print):
         if cfg.MODEL.MULTI.MULTI_PPNET_PATH != "NA":
             if cfg.DATASET.GENETIC.PPNET_PATH != "NA" or cfg.DATASET.IMAGE.PPNET_PATH != "NA":
                 log("Warning: Loading MultiModalNetwork, other pretrained networks are ignored...")
-            multi.load_state_dict(torch.load(cfg.MODEL.MULTI.MULTI_PPNET_PATH))
+            # multi.load_state_dict(torch.load(cfg.MODEL.MULTI.MULTI_PPNET_PATH))
+            # Load from file, not from state_dict
+            multi = torch.load(cfg.MODEL.MULTI.MULTI_PPNET_PATH)
         return multi

train_and_test.py
@@ -20,12 +20,11 @@ def CE(logits,target):
      return torch.sum(torch.sum(- target * torch.log(probs))) 
 
 def get_cluster_and_sep_cost(min_distances, target, num_classes):
-    target_one_hot = torch.zeros(target.size(0), num_classes).cuda()
+    target_one_hot = torch.zeros(target.size(0), num_classes).to("cuda")
     make_one_hot(target - 1, target_one_hot)
     num_prototypes_per_class = min_distances.size(1) // num_classes
     one_hot_repeat = target_one_hot.unsqueeze(2).repeat(1,1,num_prototypes_per_class).\
                         view(target_one_hot.size(0),-1)
-    
     cluster_cost = torch.mean(torch.min(min_distances * one_hot_repeat, dim=1)[0])
 
     flipped_one_hot_repeat = 1 - one_hot_repeat
@@ -37,7 +36,7 @@ def get_cluster_and_sep_cost(min_distances, target, num_classes):
 
 def get_l1_cost(node):
     l1_mask = (
-        1 - torch.t(node.prototype_class_identity).cuda()
+        1 - torch.t(node.prototype_class_identity).to("cuda")
     )
     l1 = torch.linalg.vector_norm(
         node.last_layer.weight * l1_mask, ord=1
@@ -47,7 +46,7 @@ def get_l1_cost(node):
 
 def get_multi_last_layer_l1_cost(node):
     l1_mask = (
-        1- torch.t(node.logit_class_identity).cuda()
+        1- torch.t(node.logit_class_identity).to("cuda")
     )
     l1 = torch.linalg.vector_norm(
         node.multi_last_layer.weight * l1_mask, ord=1
@@ -94,6 +93,7 @@ def get_conditional_prob_accuracies(
 
     If global_ce is true, the cross entropy is calculated using the conditional probabilities of each class.
     """
+    
     recursive_put_accuracy_probs(
         conv_features,
         model.root,
@@ -102,6 +102,7 @@ def get_conditional_prob_accuracies(
         parallel_mode,
         scale = (1,1) if parallel_mode else 1
     )
+    
 
     cpu_target = target.cpu()
 
@@ -285,6 +286,8 @@ def recursive_put_accuracy_probs(
         node.accu_probs = torch.nn.functional.softmax(node(conv_features)[0], dim=1) * scale
         scale = node.accu_probs[:, -1].unsqueeze(1)
 
+
+
     for c_node in node.child_nodes:
         i = c_node.int_location[-1] - 1
         recursive_put_accuracy_probs(
@@ -460,8 +463,10 @@ def recursive_get_loss(
         cross_entropy = 0
     else:
         cross_entropy = torch.nn.functional.cross_entropy(logits[mask], target[mask][:, level] - 1)
+
     cluster_cost, separation_cost = get_cluster_and_sep_cost(
-        min_distances[mask], target[mask][:, level], logits.size(1))
+        min_distances[mask], target[mask][:, level], logits.size(1)
+    )
 
     l1_cost = get_l1_cost(node)
 
@@ -476,11 +481,10 @@ def recursive_get_loss(
         class_correct = correct[class_mask]
         accuracy_tree["children"][i]["correct"] += class_correct.sum()
         accuracy_tree["children"][i]["total"] += class_mask.sum()
-        
+    
     for c_node in node.child_nodes:
         i = c_node.int_location[-1] - 1
 
-        c_logits, c_min_distances = c_node(conv_features)
         applicable_mask = target[:,level] - 1 == i
         
         if applicable_mask.sum() == 0:
@@ -529,12 +533,25 @@ def construct_accuracy_tree(root):
             construct_accuract_tree_rec(child) for child in root.all_child_nodes
         ]}
 
-def _train_or_test(model, dataloader, global_ce, parallel_mode, optimizer=None, coefs = None, class_specific=False, log=print, warm_up = False, CEDA = False, batch_mult = 1, class_acc = False):
+def _train_or_test(
+        model,
+        dataloader,
+        global_ce,
+        parallel_mode,
+        optimizer=None,
+        coefs = None,
+        class_specific=False,
+        log=print,
+        warm_up = False,
+        CEDA = False,
+        batch_mult = 1,
+        class_acc = False):
     '''
     model: the multi-gpu model
     dataloader:
     optimizer: if None, will be test evaluation
     '''
+    
     is_train = optimizer is not None
     
     if not is_train: 
@@ -554,6 +571,8 @@ def _train_or_test(model, dataloader, global_ce, parallel_mode, optimizer=None, 
     else:
         accuracy_tree = construct_accuracy_tree(model.module.root)
 
+    
+
     total_cross_entropy = 0
     total_cluster_cost = 0
     total_separation_cost = 0
@@ -563,15 +582,15 @@ def _train_or_test(model, dataloader, global_ce, parallel_mode, optimizer=None, 
     
     for i, ((genetics, image), label) in enumerate(dataloader):
         if model.module.mode == 1:
-            input = genetics.cuda()
+            input = genetics.to("cuda")
         elif model.module.mode == 2:
-            input = image.cuda()
+            input = image.to("cuda")
         else:
-            input = (genetics.cuda(), image.cuda())
+            input = (genetics.to("cuda"), image.to("cuda"))
             # raise NotImplementedError("Multimodal not implemented")
-        
+
         target = label.type(torch.LongTensor)
-        target = target.cuda()
+        target = target.to("cuda")
 
         batch_size = len(target)
         batch_start = time.time()   
@@ -594,13 +613,14 @@ def _train_or_test(model, dataloader, global_ce, parallel_mode, optimizer=None, 
 
         with grad_req:
             conv_features = model.module.conv_features(input)
+            
             if model.module.mode == 3:
                 # Freeze last layer multi
                 cross_entropy, cluster_cost, separation_cost, l1, num_parents_in_batch, summed_correspondence_cost, summed_correspondence_cost_count = recursive_get_loss_multi( 
                     conv_features=conv_features,
                     node=model.module.root,
                     target=target,
-                    prev_mask=torch.ones(batch_size, dtype=bool).cuda(),
+                    prev_mask=torch.ones(batch_size, dtype=bool).to("cuda"),
                     level=0,
                     global_ce=global_ce,
                     correct_arr=correct_arr,
@@ -614,7 +634,7 @@ def _train_or_test(model, dataloader, global_ce, parallel_mode, optimizer=None, 
                     conv_features=conv_features,
                     node=model.module.root,
                     target=target,
-                    prev_mask=torch.ones(batch_size, dtype=bool).cuda(),
+                    prev_mask=torch.ones(batch_size, dtype=bool).to("cuda"),
                     level=0,
                     global_ce=global_ce,
                     correct_arr=correct_arr,
@@ -686,7 +706,7 @@ def _train_or_test(model, dataloader, global_ce, parallel_mode, optimizer=None, 
             loss.backward()
             
             # if CEDA:
-            #     noise = torch.stack([normalize(torch.rand((3,32,32)), mean, std) for n in range(batch_size)]).cuda()
+            #     noise = torch.stack([normalize(torch.rand((3,32,32)), mean, std) for n in range(batch_size)]).to("cuda")
             #     _ = model(noise) 
             #     for node in model.module.root.nodes_with_children():
             #         noise_cross_ent += 1 * CE(node.logits,node.unif) # 1/10
@@ -750,15 +770,24 @@ def _train_or_test(model, dataloader, global_ce, parallel_mode, optimizer=None, 
 def train(model, dataloader, optimizer, coefs, parallel_mode, global_ce=True, class_specific=False, log=print, warm_up = False):
     assert(optimizer is not None)
     log('train')
-    return _train_or_test(model=model, dataloader=dataloader, parallel_mode=parallel_mode, global_ce=global_ce, optimizer=optimizer, coefs=coefs,
-                          class_specific=class_specific, log=log, warm_up = warm_up, CEDA=coefs['CEDA'])
+    return _train_or_test(
+        model=model,
+        dataloader=dataloader,
+        parallel_mode=parallel_mode,
+        global_ce=global_ce,
+        optimizer=optimizer,
+        coefs=coefs,
+        class_specific=class_specific,
+        log=log,
+        warm_up = warm_up,
+        CEDA=coefs['CEDA'])
 
-def valid(model, dataloader, coefs = None, parallel_mode=True, class_specific=False, log=print):
+def valid(model, dataloader, coefs = None, parallel_mode=False, class_specific=False, log=print):
     log('valid')
     return _train_or_test(model=model, dataloader=dataloader, parallel_mode=parallel_mode, optimizer=None, coefs=coefs,
                           class_specific=class_specific, log=log)
 
-def test(model, dataloader, coefs = None, parallel_mode=True, global_ce=True, class_specific=False, log=print, class_acc = False):
+def test(model, dataloader, coefs = None, parallel_mode=False, global_ce=True, class_specific=False, log=print, class_acc = False):
     #log('test')
     return _train_or_test(model=model, dataloader=dataloader, parallel_mode=parallel_mode, global_ce=global_ce, optimizer=None, coefs = coefs,
                       class_specific=class_specific, log=log, class_acc=class_acc)
@@ -766,7 +795,7 @@ def test(model, dataloader, coefs = None, parallel_mode=True, global_ce=True, cl
 def make_one_hot(target, target_one_hot):
     target_copy = torch.LongTensor(len(target))
     target_copy.copy_(target)
-    target_copy = target_copy.view(-1,1).cuda()
+    target_copy = target_copy.view(-1,1).to("cuda")
     target_one_hot.zero_()
     target_one_hot.scatter_(dim=1, index=target_copy, value=1.)
 
@@ -777,9 +806,9 @@ def auxiliary_costs(label,num_prototypes_per_class,num_classes,prototype_shape,m
     
     max_dist = prototype_shape[1] * prototype_shape[2] * prototype_shape[3]
 
-    target = label.cuda()
+    target = label.to("cuda")
     target_one_hot = torch.zeros(target.size(0), num_classes)
-    target_one_hot = target_one_hot.cuda()                
+    target_one_hot = target_one_hot.to("cuda")                
     make_one_hot(target, target_one_hot)
     one_hot_repeat = target_one_hot.unsqueeze(2).repeat(1,1,num_prototypes_per_class).\
                         view(target_one_hot.size(0),-1)