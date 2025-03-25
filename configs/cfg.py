787ebbe72cfa9cb59d04eb1748d3cffd6a3c7c98
mbahng <bahngmc@gmail.com>
2025-03-25 17:16:13 -0400

    more cleanup of yaml files

--- configs/cfg.py
+++ configs/cfg.py
@@ -2,7 +2,7 @@ from yacs.config import CfgNode as CN
 
 _C = CN()
 
-_C.RUN_NAME = "" # Name of the run. If "", will be set to the current time.
+_C.RUN_NAME = "" 
 _C.SEED = 2024
 _C.WANDB_MODE = "online"
 
@@ -19,8 +19,6 @@ _C.MODEL.PRUNING_TAU = 3
 _C.MODEL.MULTI = CN()
 _C.MODEL.MULTI.MULTI_PPNET_PATH = "NA"
 
-# _C.MODEL.PROTOTYPE_DISTANCE_FUNCTION = 'cosine'
-# _C.MODEL.PROTOTYPE_ACTIVATION_FUNCTION = 'linear'
 _C.MODEL.GENETIC_MODE = False
 
 # Dataset
@@ -28,15 +26,14 @@ _C.DATASET = CN()
 _C.DATASET.DATA_FILE = "../datasets/source_files/metadata_cleaned_permissive.tsv" # Path to CSV from which data can be selected
 _C.DATASET.IMAGE_PATH = "../datasets/full_bioscan_images/" # Path to image directory
 _C.DATASET.TREE_SPECIFICATION_FILE = "NA" # Path to JSON file that specifies tree structure
-_C.DATASET.TRAIN_NOT_CLASSIFIED_PROPORTIONS = [0,0,.25,.5] # Proportions of samples at each level that are unclassified [order, family, genus, species]. Note: Lower levels counts do not consider higher level counts, so for this default, > 50% of species are unclassified (50% + 25% of genus)
-_C.DATASET.FLAT_CLASS = True # If true, the dataset will be flattened to only include species. If false, the dataset will include all levels.
+_C.DATASET.TRAIN_NOT_CLASSIFIED_PROPORTIONS = [0,0,0,0] # Proportions of samples at each level that are unclassified [order, family, genus, species]. Note: Lower levels counts do not consider higher level counts, so for this default, > 50% of species are unclassified (50% + 25% of genus)
 
 _C.DATASET.MODE = 3 # 0 is illegal, don't use. 1 is genetic only, 2 is image only, 3 is joint. This will only affect what the dataloader/dataset returns. Not the augmentation.
 _C.DATASET.PARALLEL_MODE = False # If true, the image and genetic models will train separately with a shared correspondence loss. If false, the models will train jointly. Only relevant if MODE = 3.
 
 _C.DATASET.CACHED_DATASET_FOLDER = "" # Path to folder with pre-existing datasets. Most other dataset parameters will be ignored if this is set. "" for no cache.  
 
-_C.DATASET.TRAIN_VAL_TEST_SPLIT = (120, 40, 40) # For each leaf node, the number of samples in the training, validation, and test sets.
+_C.DATASET.TRAIN_VAL_TEST_SPLIT = (0, 0, 0) # For each leaf node, the number of samples in the training, validation, and test sets.
 
 _C.DATASET.PREEXISTING = False # Whether the dataset has already preprocessed (augmentaiton has occured)
 
@@ -94,36 +91,35 @@ _C.OPTIM.COEFS.IMAGE.SEP = 0.
 _C.OPTIM.COEFS.IMAGE.L1 = 0.
 _C.OPTIM.COEFS.IMAGE.ORTHO = 0.
 
-_C.OPTIM.COEFS.CORRESPONDENCE = -0.
+_C.OPTIM.COEFS.CORRESPONDENCE = 0.
 
 # Warm optimizer
 _C.OPTIM.WARM = CN()
-_C.OPTIM.WARM.ADD_ON_LAYERS_LR = -1.
-_C.OPTIM.WARM.ADD_ON_LAYERS_WD = -1.
-_C.OPTIM.WARM.PROTOTYPE_LR = -1.
-_C.OPTIM.WARM.PROTOTYPE_WD = -1.
+_C.OPTIM.WARM.ADD_ON_LAYERS_LR = 0.
+_C.OPTIM.WARM.ADD_ON_LAYERS_WD = 0.
+_C.OPTIM.WARM.PROTOTYPE_LR = 0.
+_C.OPTIM.WARM.PROTOTYPE_WD = 0.
 
 # Joint optimizer
 _C.OPTIM.JOINT = CN() 
-_C.OPTIM.JOINT.FEATURES_LR = -1.
-_C.OPTIM.JOINT.FEATURES_WD = -1.
-_C.OPTIM.JOINT.ADD_ON_LAYERS_LR = -1.
-_C.OPTIM.JOINT.ADD_ON_LAYERS_WD = -1.
-_C.OPTIM.JOINT.LAST_LAYER_LR = -1.
-_C.OPTIM.JOINT.LAST_LAYER_WD = -1.
-_C.OPTIM.JOINT.PROTOTYPE_LR = -1.
-_C.OPTIM.JOINT.PROTOTYPE_WD = -1.
+_C.OPTIM.JOINT.FEATURES_LR = 0.
+_C.OPTIM.JOINT.FEATURES_WD = 0.
+_C.OPTIM.JOINT.ADD_ON_LAYERS_LR = 0.
+_C.OPTIM.JOINT.ADD_ON_LAYERS_WD = 0.
+_C.OPTIM.JOINT.LAST_LAYER_LR = 0.
+_C.OPTIM.JOINT.LAST_LAYER_WD = 0.
+_C.OPTIM.JOINT.PROTOTYPE_LR = 0.
+_C.OPTIM.JOINT.PROTOTYPE_WD = 0.
 
 # Last layer optimizer
 _C.OPTIM.LAST_LAYER= CN()
-_C.OPTIM.LAST_LAYER.LAST_LAYER_LR = -1.
-_C.OPTIM.LAST_LAYER.LAST_LAYER_MOM = -1.
+_C.OPTIM.LAST_LAYER.LAST_LAYER_LR = 0.
+_C.OPTIM.LAST_LAYER.LAST_LAYER_MOM = 0.
 
 
 # Deprecated (?)
 # If true, the crossentropy term will be applied to a large vector corresponding to conditional probabilities of each species (or higher level if not classified).
 # Otherwise, the crossentropy term will be applied to each classification task and summed.
-_C.OPTIM.GLOBAL_CROSSENTROPY = True
 _C.OPTIM.CORRESPONDENCE_TYPE = "Batched" # One of ("Batched", "Single"). Batched evaluates the correspondence accross an entire batch.
 _C.OPTIM.PRUNE_START = 10
 
