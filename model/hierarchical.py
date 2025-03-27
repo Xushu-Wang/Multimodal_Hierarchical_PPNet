debugged wrong mapping in push stage and added weights/gradient visualizations

model/hierarchical.py
@@ -72,7 +72,7 @@ class ProtoNode(nn.Module):
                 self.match = self.init_match().cuda()
 
                 self.prototype = nn.Parameter(
-                    torch.rand((self.nprotos_total, *pshape), device="cuda"),
+                    torch.randn((self.nprotos_total, *pshape), device="cuda"),
                     requires_grad=True
                 )
 
@@ -103,16 +103,22 @@ class ProtoNode(nn.Module):
         return match
 
     @classifier_only
-    def init_push(self):
-        # all attributes regarding to push stage 
-        # saves the closest distance seen so far 
-        # add a new attribute and initialize it to be infinity
-        self.global_max_proto_sim = np.full(self.nprotos_total, np.inf)
+    def init_push(self): 
+        """
+        Initializes all attributes regarding push stage  
+        Say we have node A classifying into subclasses X, Y, Z
+        Let D = full dataset and D(A) = filtered dataset into samples landing in 
+        class A (plus whatever subclasses before)
+        global_max_proto_sim - keeps track of the cossim between the ith prototype 
+            (ith indexing over nclass * nprotos_per_class) and all relevant samples 
+            in D(A). 
+        global_max_fmap_patches - the corresponding patches in the conv outputs 
+            that goes with the global_max_proto_sim (like argmax). 
+        """
+        self.global_max_proto_sim = np.full(self.nprotos_total, -np.inf)
 
-        # saves the patch representation that gives the current smallest distance
+        # saves the patch representation that gives the greater cosine similarity. 
         self.global_max_fmap_patches = np.zeros([self.nprotos_total, *self.pshape])
-
-        # 
 
         # We assume save_prototype_class_identity is true
         '''
@@ -210,7 +216,7 @@ class ProtoNode(nn.Module):
     def cos_sim(self, x): 
         """
         x - convolutional output features: img=(80, 2048, 8, 8) gen=(80, 64, 1, 40)  
-        prototype - full prototypes in node: img=(10, 2048, 1, 1), gen=(40, 64, 1, 40)
+        prototype - full prototypes in node: img=(10, 2048, 1, 1), gen=(40, 64, 1, 40) 
         """
         # keep sqrt_D here 
         sqrt_D = (self.pshape[1] * self.pshape[2]) ** 0.5 
@@ -287,7 +293,7 @@ class HierProtoPNet(nn.Module):
             pshape - 3-tensor of prototype shapes 
             nproto - num prototypes per class
         """
-        super().__init__()
+        super(HierProtoPNet, self).__init__()
         if mode == Mode.MULTIMODAL:
             raise ValueError("Use MultiHierarchical_PPNet for joint mode")
         if len(pshape) != 3: 

model/multimodal.py
@@ -52,8 +52,8 @@ class CombinerProtoNode(nn.Module):
         self.img_node.init_push()
 
     def forward(self, gen_conv_features, img_conv_features): 
-        genetic_logit, genetic_dist = self.gen_node.forward(gen_conv_features)
-        image_logit, image_dist = self.img_node.forward(img_conv_features)
+        genetic_logit, genetic_dist = self.gen_node(gen_conv_features)
+        image_logit, image_dist = self.img_node(img_conv_features)
 
         return (genetic_logit, image_logit), (genetic_dist, image_dist)
 
@@ -78,9 +78,9 @@ class MultiHierProtoPNet(nn.Module):
         self.gen_net = gen_net
         self.img_net = img_net
 
-        self.mode = Mode.MULTIMODAL
-        self.all_classifier_nodes = nn.ModuleList()
-        self.classifier_nodes = nn.ModuleList()
+        self.mode = Mode.MULTIMODAL 
+        self.all_classifier_nodes = nn.ModuleList()     # non-leaf nodes
+        self.classifier_nodes = nn.ModuleList()         # nontrivial (i.e. not 1 class) classifiers nodes
         self.root = self.build_combiner_proto_tree(self.gen_net.root, self.img_net.root)
         self.add_on_layers = nn.ModuleList([self.gen_net.add_on_layers, self.img_net.add_on_layers])
 

train/train_and_test.py
@@ -102,7 +102,7 @@ def _traintest_genetic(model, dataloader, optimizer, cfg):
             for node in model.classifier_nodes: 
                 # filter out the irrelevant samples in batch 
                 mask = torch.all(label[:,:node.depth] == node.idx.cuda(), dim=1) 
-                logits, max_sim = node.forward(conv_features)
+                logits, max_sim = node(conv_features)
                 
                 # softmax the nodes to initialize node.probs
                 node.softmax() 
@@ -176,7 +176,7 @@ def _traintest_image(model, dataloader, optimizer, cfg):
             for node in model.classifier_nodes: 
                 # filter out the irrelevant samples in batch 
                 mask = torch.all(label[:,:node.depth] == node.idx.cuda(), dim=1) 
-                logits, max_sim = node.forward(conv_features)
+                logits, max_sim = node(conv_features)
                 
                 # softmax the nodes to initialize node.probs
                 node.softmax() 
@@ -252,8 +252,8 @@ def _traintest_multi(model, dataloader, optimizer, cfg):
             for node in model.classifier_nodes: 
                 # filter out the irrelevant samples in batch 
                 mask = torch.all(label[:,:node.depth] == node.idx.cuda(), dim=1) 
-                gen_logits, gen_max_sim = node.gen_node.forward(gen_conv_features) 
-                img_logits, img_max_sim = node.img_node.forward(img_conv_features) 
+                gen_logits, gen_max_sim = node.gen_node(gen_conv_features) 
+                img_logits, img_max_sim = node.img_node(img_conv_features) 
                 node.gen_node.softmax()
                 node.img_node.softmax() 
 